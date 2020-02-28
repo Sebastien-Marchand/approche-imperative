971e06d860509a58572f8fa8a6c462b3e19638d5
Sébastien Marchand <sebastien.marchand78@gmail.com>
2020-02-28 12:28:01 +0100

    TP-Declaration de Variable

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -4,6 +4,16 @@ public class DeclarationApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		byte monbyte = 1;
+		short monshort = 2;
+		int monint = 3;
+		long monlong = 1_000_000l;
+		float monfloat = 1.0f;
+		double mondouble = 2.0d;
+		char monchar = 'A';
+		boolean monbool = true;
+		System.out.println("Monbyte: " +monbyte+"\nMon short: " +monshort+"\nMonint: "+monint+"\nMonlong: " +monlong
+				+"\nMonfloat: " +monfloat+"\nMondouble: "+mondouble+ "\nMonchar: "+monchar+"\nMonbool: "+monbool);
 
 	}
 
