ea9de73914408a35e675c8291b5ae9a03cbda80c
Sébastien Marchand <sebastien.marchand78@gmail.com>
2020-03-02 19:25:58 +0100

    1e version TP J-2 JAVA

--- src/fr/algorithmie/RechercheMax.java
+++ src/fr/algorithmie/RechercheMax.java
@@ -4,12 +4,12 @@ public class RechercheMax {
 
 	public static void main(String[] args) {
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
-		int tailleTableau = array.length;																		//taille tableau array
+		int tailleTableau = array.length;
 		
-		int maPlusGrandeValeur = array[0];
+		int maPlusGrandeValeur = array[0];																		//initialise la 1e val du tableau
 		
-				// Affiche l'ensemble du tableau
-		System.out.println("\t Affichage dans l'ordre:\n");
+																												// détermine la plus grande val du tableau
+		System.out.println("\t Affichage dans l'ordre:\n");														//en comparant la valeur précédente
 		for (int i = 1 ; i < tailleTableau ; i++)
 		{
 			if (maPlusGrandeValeur < array[i])
@@ -18,7 +18,7 @@ public class RechercheMax {
 			}
 		}
 		
-		System.out.println("La plus grand valeur est :"+maPlusGrandeValeur);									//Affichage de la valeur
+		System.out.println("La plus grand valeur est :"+maPlusGrandeValeur);									//Affichage de la valeur la + grande
 
 	}
 
